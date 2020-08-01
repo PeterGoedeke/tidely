59924338e2e93628982cfc2661c16e41e5aef2c4
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-01 16:40:43 +1200

    Tides returns detailed tide information for the current day

--- controllers/tides.js
+++ controllers/tides.js
@@ -1,24 +1,39 @@
 const axios = require('axios')
 
-async function tide({ lat, long }) {
+async function getTides({ lat, long }, today = false) {
     try {
         const response = await axios.get(process.env.NIWA_URL, {
             params: {
                 apikey: process.env.NIWA_API_KEY,
                 lat,
-                long
+                long,
+                interval: today ? 10 : undefined,
+                numberOfDays: today ? 1 : undefined
             },
         })
         // retrieve the useful information from the api call and return
-        const tidePoints = response.data.values
-        return nextLowAndHigh(tidePoints.slice(0,2))
+        const tideInfo = response.data.values
+        return tideInfo
     }
     catch (err) {
         return err
     }
 }
 
-function nextLowAndHigh(nextTides) {
+async function tides(latLng) {
+    const today = getTides(latLng, true)
+    const weekPeaks = getTides(latLng)
+
+    const response = await Promise.all([ today, weekPeaks ])
+
+    return {
+        nextLowAndHigh: getNextLowAndHigh(response[1].slice(0,2)),
+        today: response[0],
+        weekPeaks: response[1]
+    }
+}
+
+function getNextLowAndHigh(nextTides) {
     let low, high = null
     if(nextTides[0].value > nextTides[1].value) {
         low = nextTides[1]
@@ -31,4 +46,4 @@ function nextLowAndHigh(nextTides) {
     return { low, high }
 }
 
-module.exports = tide
+module.exports = tides
