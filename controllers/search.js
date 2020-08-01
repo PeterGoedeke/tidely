09e06aa79fe9752dc2487d035ff0d9e1bec300c7
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-01 14:16:38 +1200

    Added basic endpoint to get tide information

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,10 @@
-function search(req, res) {
+const geocode = require('./geocode')
+const tides = require('./tides')
 
+async function search(req, res) {
+    const latLng = await geocode(req.params.search)
+    const tideInfo = await tides(latLng)
+    return res.status(200).json(tideInfo)
 }
 
 module.exports = search
