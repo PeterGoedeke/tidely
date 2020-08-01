29d0617df7976ac3bf51a29db79f4d59a500871a
Michael Shaimerden <sanzharshaimerden@gmail.com>
2020-08-01 15:57:46 +1200

    Start location autocomplete

--- front-end/src/containers/LocationSearch.js
+++ front-end/src/containers/LocationSearch.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import classes from './LocationSearch.module.css';
 
+import { connect } from "react-redux";
+import { Redirect, withRouter } from "react-router-dom";
 
+import * as actions from "../store/actions/index";
+import classes from "./LocationSearch.module.css";
+
+import * as locations from "../assets/beaches/beaches.json";
 
 class LocationSearch extends Component {
   state = {};
 
   componentDidMount() {
     //
+    console.log(locations[0]);
   }
 
   submitLocation = (event) => {
@@ -20,4 +26,21 @@ class LocationSearch extends Component {
     return <div>Hello World</div>;
   }
 }
-export default LocationSearch;
+
+const mapStateToProps = (state) => {
+  return {
+    loading: state.tideData.loading,
+    error: state.tideData.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchTideData: (inputLocation) =>
+      dispatch(actions.fetchTideData(inputLocation)),
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(LocationSearch)
+);
