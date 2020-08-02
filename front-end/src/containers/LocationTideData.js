4d1ffb8c9446cb85a465d88cb98d36a6f1c9dc90
Michael Shaimerden <sanzharshaimerden@gmail.com>
2020-08-02 13:40:21 +1200

    Add top bar to tide data page

--- front-end/src/containers/LocationTideData.js
+++ front-end/src/containers/LocationTideData.js
@@ -1,25 +1,62 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
-import { Redirect, withRouter } from "react-router-dom";
 
-import * as actions from "../store/actions/index";
 import classes from "./LocationTideData.module.css";
-
-import * as locations from "../assets/beaches/beaches.json";
 
 import tidelyLogo from "../assets/icons/logo.png";
 
+import SearchBar from "../components/SearchBar/SearchBar";
+
 import searchIcon from "../assets/icons/search-2.png";
+import crossIcon from "../assets/icons/cross.png";
 
 class LocationTideData extends Component {
-  state = {};
+  state = {
+    showSearchOverlay: false,
+  };
 
+  showSearchOverlay = () => {
+    this.setState({
+      showSearchOverlay: true,
+    });
+  };
+
+  closeSearchOverlay = () => {
+    this.setState({
+      showSearchOverlay: false,
+    });
+  };
   render() {
+    let searchOverlay = (
+      <div className={classes.SearchOverlay}>
+        <SearchBar />
+      </div>
+    );
+    if (!this.state.showSearchOverlay) {
+      searchOverlay = null;
+    }
     return (
       <div className={classes.LocationTideData}>
+        {searchOverlay}
         <div className={classes.TopBar}>
           <img src={tidelyLogo} className={classes.Logo} alt="Tidely Logo" />
+          {!this.state.showSearchOverlay ? (
+            <div
+              className={classes.IconContainer}
+              onClick={this.showSearchOverlay}
+            >
+              <input value="Enter a Beach location..." />
+              <img src={searchIcon} className={classes.SearchIcon} />
+            </div>
+          ) : (
+            <img
+              src={crossIcon}
+              className={classes.crossIcon}
+              onClick={this.closeSearchOverlay}
+            />
+          )}
+          {/* <SearchBar /> */}
         </div>
         <div className={classes.WeatherInfo}>weather info</div>
         <div className={classes.TideInfo}>tide indfo</div>
