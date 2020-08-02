24171147a5b737de915b0e4a7736a431d43fe125
Jiaru <lin8231@outlook.com>
2020-08-02 12:04:33 +1200

    created tide info page

---
+++ front-end/src/containers/LocationTideData.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import { Redirect, withRouter } from "react-router-dom";
+
+import * as actions from "../store/actions/index";
+import classes from "./LocationTideData.module.css";
+
+import * as locations from "../assets/beaches/beaches.json";
+
+import tidelyLogo from "../assets/icons/logo.png";
+
+import searchIcon from "../assets/icons/search-2.png";
+
+class LocationTideData extends Component{
+    state = {}
+
+    render(){
+        return(
+            <div className={classes.LocationTideData}>
+                <div className={classes.TopBar}>top bar</div>
+                <div className={classes.WeatherInfo}>weather info</div>
+                <div className={classes.TideInfo}>tide indfo</div>
+            </div>
+        )
+    }
+
+}
+
+export default LocationTideData;
